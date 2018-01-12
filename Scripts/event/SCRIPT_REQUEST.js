e8dcff54af42ba717911c336c71bbe15b24023be
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-13 01:49:47 +0700

    Test GS scripts

---
+++ Scripts/event/SCRIPT_REQUEST.js
@@ -0,0 +1,9 @@
+// ====================================================================================================
+//
+// Cloud Code for SCRIPT_REQUEST, write your code here to customize the GameSparks platform.
+//
+// For details of the GameSparks Cloud Code API see https://docs.gamesparks.com/
+//
+// ====================================================================================================
+
+var test = "test";
