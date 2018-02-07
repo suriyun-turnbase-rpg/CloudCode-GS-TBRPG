f28853c8666c6a4a1729f2f8376064b3c742ea42
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-02-08 04:12:42 +0700

    Add requiring database enum/data

--- event/SERVICE_EVENT.js
+++ event/SERVICE_EVENT.js
@@ -26,3 +26,9 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 // ====================================================================================================
+require("GAME_DATA_ENUM");
+require("GAME_DATA");
+var logger = Spark.getLog();
+var gsData = Spark.getData();
+var target = gsData.TARGET;
+var data = gsData.DATA;
