031a30f4557320796ed5ace25c2c41527f851963
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-01-24 21:03:07 +0700

    Complete buy virtual good codes

--- response/BuyVirtualGoodResponse.js
+++ response/BuyVirtualGoodResponse.js
@@ -34,69 +34,86 @@ require("HELPERS");
 var API = Spark.getGameDataService();
 var player = Spark.getPlayer();
 var playerId = player.getPlayerId();
-var iapPackageId = Spark.getData().iapPackageDataId;
-var iapPackage = gameDatabase.iapPackages[iapPackageId];
-if (!iapPackage)
+var gsData = Spark.getData();
+var boughtItems = gsData.boughtItems;
+    
+// Reward result
+var allRewardItems = [];
+var createItems = [];
+var updateItems = [];
+var deleteItemIds = [];
+var updateCurrencies = [];
+
+var boughtItemsCount = boughtItems.length;
+for (var boughtItemCounter = 0; boughtItemCounter < boughtItemsCount; ++boughtItemCounter)
 {
-    Spark.setScriptData("error", ERROR_INVALID_IAP_PACKAGE_DATA);
-}
-else
-{
-    // Reward data
-    var rewardSoftCurrency = iapPackage.rewardSoftCurrency;
-    var rewardHardCurrency = iapPackage.rewardHardCurrency;
-    var rewardItems = iapPackage.rewardItems;
-    
-    // Reward result
-    var createItems = [];
-    var updateItems = [];
-    var deleteItemIds = [];
-    var updateCurrencies = [];
-    
-    // Soft currency
-    player.credit(gameDatabase.currencies.SOFT_CURRENCY, rewardSoftCurrency, "IAP Bought [" + iapPackageId + "]");
-    var softCurrency = GetCurrency(playerId, gameDatabase.currencies.SOFT_CURRENCY);
-    updateCurrencies.push(softCurrency);
-    
-    // Hard currency
-    player.credit(gameDatabase.currencies.HARD_CURRENCY, rewardHardCurrency, "IAP Bought [" + iapPackageId + "]");
-    var hardCurrency = GetCurrency(playerId, gameDatabase.currencies.HARD_CURRENCY);
-    updateCurrencies.push(hardCurrency);
-    
-    var countRewardItems = rewardItems.length;
-    for (var i = 0; i < countRewardItems; ++i)
+    var boughtItem = boughtItems[boughtItemCounter];
+    var iapPackageId = boughtItem.shortCode;
+    var boughtQuantity = boughtItem.quantity;
+    var iapPackage = gameDatabase.iapPackages[iapPackageId];
+    if (!iapPackage)
     {
-        var addItemsResult = AddItems(playerId, rewardItem.id, rewardItem.amount);
-        if (addItemsResult.success)
-        {
-            var countCreateItems = addItemsResult.createItems.length;
-            var countUpdateItems = addItemsResult.updateItems.length;
-            for (var j = 0; j < countCreateItems; ++j)
-            {
-                var createItem = addItemsResult.createItems[j];
-                var newItemEntry = API.createItem(colPlayerItem, createItem.id);
-                newItemEntry.setData(createItem);
-                newItemEntry.persistor().persist().error();
-                HelperUnlockItem(playerId, createItem.dataId);
-                createItems.push(createItem);
-            }
-            for (var j = 0; j < countUpdateItems; ++j)
-            {
-                var updateItem = addItemsResult.updateItem[j];
-                var updateItemResult = API.getItem(colPlayerItem, updateItem.id);
-                var updateItemEntry = updateItemResult.document();
-                updateItemEntry.setData(updateItem);
-                updateItemEntry.persistor().persist().error();
-                updateItems.push(updateItem);
-            }
-        }
+        continue;
     }
     
-    // Set data which will be sent to clients
-    Spark.setScriptData("rewardItems", rewardItems);
-    Spark.setScriptData("createItems", createItems);
-    Spark.setScriptData("updateItems", updateItems);
-    Spark.setScriptData("deleteItemIds", deleteItemIds);
-    Spark.setScriptData("updateCurrencies", updateCurrencies);
-    Spark.setScriptData("player", GetPlayer(playerId));
+    for (var boughtQuantityCounter = 0; boughtQuantityCounter < boughtQuantity; ++boughtQuantityCounter)
+    {
+        // Reward data
+        var rewardSoftCurrency = iapPackage.rewardSoftCurrency;
+        var rewardHardCurrency = iapPackage.rewardHardCurrency;
+        var rewardItems = iapPackage.rewardItems;
+        
+        // Soft currency
+        player.credit(gameDatabase.currencies.SOFT_CURRENCY, rewardSoftCurrency, "IAP Bought [" + iapPackageId + "]");
+        var softCurrency = GetCurrency(playerId, gameDatabase.currencies.SOFT_CURRENCY);
+        updateCurrencies.push(softCurrency);
+        
+        // Hard currency
+        player.credit(gameDatabase.currencies.HARD_CURRENCY, rewardHardCurrency, "IAP Bought [" + iapPackageId + "]");
+        var hardCurrency = GetCurrency(playerId, gameDatabase.currencies.HARD_CURRENCY);
+        updateCurrencies.push(hardCurrency);
+        
+        var countRewardItems = rewardItems.length;
+        for (var i = 0; i < countRewardItems; ++i)
+        {
+            var rewardItem = rewardItems[i];
+            if (!rewardItem)
+            {
+                continue;
+            }
+
+            allRewardItems.push(rewardItem);
+            var addItemsResult = AddItems(playerId, rewardItem.id, rewardItem.amount);
+            if (addItemsResult.success)
+            {
+                var countCreateItems = addItemsResult.createItems.length;
+                var countUpdateItems = addItemsResult.updateItems.length;
+                for (var j = 0; j < countCreateItems; ++j)
+                {
+                    var createItem = addItemsResult.createItems[j];
+                    var newItemEntry = API.createItem(colPlayerItem, createItem.id);
+                    newItemEntry.setData(createItem);
+                    newItemEntry.persistor().persist().error();
+                    HelperUnlockItem(playerId, createItem.dataId);
+                    createItems.push(createItem);
+                }
+                for (var j = 0; j < countUpdateItems; ++j)
+                {
+                    var updateItem = addItemsResult.updateItem[j];
+                    var updateItemResult = API.getItem(colPlayerItem, updateItem.id);
+                    var updateItemEntry = updateItemResult.document();
+                    updateItemEntry.setData(updateItem);
+                    updateItemEntry.persistor().persist().error();
+                    updateItems.push(updateItem);
+                }
+            }
+        } // End add item loop
+    } // Bought quantity loop
 }
+// Set data which will be sent to clients
+Spark.setScriptData("rewardItems", allRewardItems);
+Spark.setScriptData("createItems", createItems);
+Spark.setScriptData("updateItems", updateItems);
+Spark.setScriptData("deleteItemIds", deleteItemIds);
+Spark.setScriptData("updateCurrencies", updateCurrencies);
+Spark.setScriptData("player", GetPlayer(playerId));
